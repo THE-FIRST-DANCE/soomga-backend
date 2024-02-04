65665cd8f9ab43ce40066adc11db69210ea90a1d
Juhyeon Lee <l.juhyeonni@gmail.com>
2024-02-05 07:07:58 +0900

    feat(validationPipe): validation pipe 에 대한 전역 구성 설정

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './modules/app/app.module';
 import { swaggerSetup } from './swagger';
 import { ConfigService } from '@nestjs/config';
 import { NestConfig } from './configs/config.interface';
+import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
@@ -10,6 +11,8 @@ async function bootstrap() {
     logger: ['error', 'warn', 'debug', 'log'],
   });
   app.use(cookieParser());
+
+  app.useGlobalPipes(new ValidationPipe());
 
   const configService = app.get<ConfigService>(ConfigService);
   const nestConfig = configService.get<NestConfig>('nest');
