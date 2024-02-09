6d31993884b1a1ae94d811558a803d35c6e982e2
hetame1 <poi8946@naver.com>
2024-02-10 05:37:19 +0900

    feat: cors 문제 수정

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,16 @@ import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    logger: ['error', 'warn', 'debug', 'log'],
+    // logger: ['error', 'warn', 'debug', 'log'],
   });
+
+  app.enableCors({
+    origin: 'http://localhost:5173', // specify the allowed origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // specify the allowed methods
+    allowedHeaders: 'Content-Type, Accept', // specify the allowed headers
+    credentials: true, // enable passing of cookies from the client to the server
+  });
+
   app.use(cookieParser());
 
   app.useGlobalPipes(new ValidationPipe());
@@ -19,8 +27,7 @@ async function bootstrap() {
 
   swaggerSetup(app);
 
-  await app.listen(nestConfig.port, () => {
-    console.log(`Server is running on: ${nestConfig.port}`);
-  });
+  await app.listen(nestConfig.port);
+  console.log(`Server is running on: ${nestConfig.port}`);
 }
 bootstrap();
