82a9800ab412b69be12eb1420d89e3046790c679
hetame1 <poi8946@naver.com>
2024-02-13 20:04:46 +0900

    feat: 장소 추가할때 장소 상세정보도 포함

--- src/modules/places/places.repository.ts
+++ src/modules/places/places.repository.ts
@@ -12,6 +12,7 @@ export class PlacesRepository {
     private readonly googleService: GoogleHttpService,
   ) {}
 
+  // 장소 검색
   async searchPlaces(
     query: string,
     pagetoken: string,
@@ -20,11 +21,22 @@ export class PlacesRepository {
     return this.googleService.searchPlaces(query, pagetoken, location);
   }
 
+  // 장소 상세 정보
+  async getDetail(placeId: string) {
+    const placeDetail = await this.googleService.getPlaceDetail(placeId);
+
+    return placeDetail;
+  }
+
+  // 장소 목록
   async getPlaces(category: string, region: string) {
     if (category === 'all') {
       return await this.prismaService.place.findMany({
         where: {
           region,
+        },
+        include: {
+          openingHours: true,
         },
       });
     }
@@ -34,9 +46,13 @@ export class PlacesRepository {
         category,
         region,
       },
+      include: {
+        openingHours: true,
+      },
     });
   }
 
+  // 장소 추가
   async addPlace(place: PlaceAddDto): Promise<Place> {
     const placeExist = await this.prismaService.place.findUnique({
       where: {
@@ -47,6 +63,8 @@ export class PlacesRepository {
     if (placeExist) {
       throw new Error('Place already exist');
     }
+
+    const placeDetail = await this.getDetail(place.placeId);
 
     const createPlace = await this.prismaService.place.create({
       data: {
@@ -59,8 +77,34 @@ export class PlacesRepository {
         latitude: place.latitude,
         longitude: place.longitude,
         region: place.region,
+        url: placeDetail.result.url,
+        phone: placeDetail.result.formatted_phone_number ?? '',
+        detailAddress: placeDetail.result.formatted_address,
       },
     });
+
+    if (placeDetail.result.opening_hours) {
+      console.log(placeDetail.result.opening_hours.periods);
+
+      const periods = placeDetail.result.opening_hours.periods.map((period) => {
+        return {
+          close: period.close?.time ?? '',
+          open: period.open.time,
+          day: period.open.day,
+        };
+      });
+
+      await this.prismaService.openingHours.createMany({
+        data: periods.map((period) => {
+          return {
+            closeTime: period.close,
+            openTime: period.open,
+            dayOfWeek: period.day,
+            placeId: createPlace.id,
+          };
+        }),
+      });
+    }
 
     return createPlace;
   }
