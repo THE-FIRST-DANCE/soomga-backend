877928121c741506ee762314fe236b43333113df
hetame1 <poi8946@naver.com>
2024-02-21 20:23:24 +0900

    refactor: 리뷰 확인 후 수정
    에러 핸들링 추가, forEach find 제거

--- src/modules/common/http/ortools.service.ts
+++ src/modules/common/http/ortools.service.ts
@@ -5,29 +5,37 @@ import { OrtoolsPostData } from 'src/interfaces/ortools.interface';
 @Injectable()
 export class OrtoolsService {
   async getRoute(data: OrtoolsPostData[]) {
-    const response = await axios.post('http://127.0.0.1:5000/distance', data);
+    try {
+      const response = await axios.post('http://127.0.0.1:5000/distance', data);
 
-    const result = [];
+      // 결과를 저장할 배열
+      const result = [];
+      const dataIndexMap = new Map(data.map((item) => [item.index, item]));
 
-    for (let i = 0; i < response.data.length; i++) {
-      const element = response.data[i];
-      const destination = data.find((d) => d.index === response.data[i + 1]);
+      // response.data의 각 요소에 대해 처리
+      response.data.forEach((element, i) => {
+        const nextElement = response.data[i + 1];
+        const current = dataIndexMap.get(element);
+        const next = dataIndexMap.get(nextElement);
 
-      if (destination) {
-        data.forEach((d) => {
-          if (d.index === element && d.destination === destination.origin) {
-            result.push({
-              id: d.placeId,
-              placeId: d.origin,
-              nextTime: d.durationTime ? d.durationTime : d.duration,
-              nextPlaceId: destination.placeId,
-              nextPlaceGoogleId: destination.origin,
-            });
-          }
-        });
-      }
+        if (current && next) {
+          result.push({
+            id: current.placeId,
+            placeId: current.origin,
+            nextTime: current.durationTime
+              ? current.durationTime
+              : current.duration,
+            nextPlaceId: next.placeId,
+            nextPlaceGoogleId: next.origin,
+          });
+        }
+      });
+
+      return result;
+    } catch (error) {
+      // 네트워크 요청 실패 또는 기타 에러를 처리
+      console.error('Failed to fetch route data:', error);
+      throw error;
     }
-
-    return result;
   }
 }
