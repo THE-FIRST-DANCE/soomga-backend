229381e41fda803a30a395be91310aa8ade74adc
hetame1 <poi8946@naver.com>
2024-02-27 19:34:21 +0900

    feat: 플랜 가져오기, 경로 수정, 경로 저장 작업

--- src/plans/plans.service.ts
+++ src/plans/plans.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { PlansRepository } from './plans.repository';
 import { PlanDistance } from 'src/interfaces/google.interface';
+import { PlanAddDto } from './dto/plans.dto';
 
 @Injectable()
 export class PlansService {
   constructor(private readonly plansRepository: PlansRepository) {}
+
+  // 플랜 가져오기
+  async getPlan(authorId: number) {
+    return await this.plansRepository.getPlan(authorId);
+  }
 
   // 경로 계산
   async getAllDistance(data: PlanDistance) {
@@ -12,5 +18,15 @@ export class PlansService {
       return await this.plansRepository.getBusDistance(data);
     }
     return await this.plansRepository.getCarDistance(data);
+  }
+
+  // 경로 수정
+  async editPlaceOrder(data: PlanDistance) {
+    return await this.plansRepository.editPlaceOrder(data);
+  }
+
+  // 경로 저장
+  async savePlan(data: PlanAddDto) {
+    return await this.plansRepository.saveSchedule(data);
   }
 }
