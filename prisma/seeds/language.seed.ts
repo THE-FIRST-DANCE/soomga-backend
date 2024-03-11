226b7f292000c4635281d90bd8a9d62aa0596786
Juhyeon Lee <l.juhyeonni@gmail.com>
2024-03-11 19:08:28 +0900

    feat(seeds): member 시드 추가 faker 라이브러리를 사용하여 테스트 데이터 입력

--- prisma/seeds/language.seed.ts
+++ prisma/seeds/language.seed.ts
@@ -1,48 +1,73 @@
-import { PrismaClient } from '@prisma/client';
+import { Prisma } from '@prisma/client';
+import { TransactionClient } from './common.interface';
 
-const languages = [
+export const korean = {
+  name: '한국어',
+  certifications: [
+    { name: 'TOPIK 1' },
+    { name: 'TOPIK 2' },
+    { name: 'TOPIK 3' },
+    { name: 'TOPIK 4' },
+    { name: 'TOPIK 5' },
+    { name: 'TOPIK 6' },
+  ],
+};
+
+export const english = {
+  name: 'English',
+  certifications: [
+    { name: 'TOEIC 900' },
+    { name: 'TOEIC 800' },
+    { name: 'TOEIC 700' },
+    { name: 'TOEIC 600' },
+    { name: 'TOEIC 500' },
+  ],
+};
+
+export const japanese = {
+  name: '日本語',
+  certifications: [
+    { name: 'JLPT N1' },
+    { name: 'JLPT N2' },
+    { name: 'JLPT N3' },
+    { name: 'JLPT N4' },
+    { name: 'JLPT N5' },
+  ],
+};
+
+const languageDatas: Prisma.LanguageCreateInput[] = [
   {
-    name: '한국어',
-    certifications: [
-      { name: 'TOPIK 1' },
-      { name: 'TOPIK 2' },
-      { name: 'TOPIK 3' },
-      { name: 'TOPIK 4' },
-      { name: 'TOPIK 5' },
-      { name: 'TOPIK 6' },
-    ],
+    name: korean.name,
+    certifications: {
+      createMany: {
+        data: korean.certifications,
+      },
+    },
   },
   {
-    name: 'English',
-    certifications: [
-      { name: 'TOEIC 900' },
-      { name: 'TOEIC 800' },
-      { name: 'TOEIC 700' },
-      { name: 'TOEIC 600' },
-      { name: 'TOEIC 500' },
-    ],
+    name: english.name,
+    certifications: {
+      createMany: {
+        data: english.certifications,
+      },
+    },
   },
   {
-    name: '日本語',
-    certifications: [
-      { name: 'JLPT N1' },
-      { name: 'JLPT N2' },
-      { name: 'JLPT N3' },
-      { name: 'JLPT N4' },
-      { name: 'JLPT N5' },
-    ],
+    name: japanese.name,
+    certifications: {
+      createMany: {
+        data: japanese.certifications,
+      },
+    },
   },
 ];
 
-export async function languageSeed(client: PrismaClient) {
-  languages.forEach(async (language) => {
-    await client.language.create({
-      data: {
-        name: language.name,
-        certifications: {
-          create: language.certifications,
-        },
-      },
-    });
-  });
+export async function languageSeed(client: TransactionClient) {
+  await Promise.all(
+    languageDatas.map((language) => {
+      return client.language.create({
+        data: language,
+      });
+    }),
+  );
 }
