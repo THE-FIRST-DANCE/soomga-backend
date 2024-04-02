1bd2aa5c4da2ac24c3c4ccad8cf913e2b81af5ee
hetame1 <poi8946@naver.com>
2024-04-02 10:27:56 +0900

    feat: dto 수정

--- src/modules/boards/sos/dto/create-sos.dto.ts
+++ src/modules/boards/sos/dto/create-sos.dto.ts
@@ -1,4 +1,7 @@
 import { OmitType } from '@nestjs/swagger';
 import { CreateBoardDto } from 'src/shared/boards/dto/create-board.dto';
 
-export class CreateSOSDto extends OmitType(CreateBoardDto, ['type'] as const) {}
+export class CreateSOSDto extends OmitType(CreateBoardDto, [
+  'type',
+  'title',
+] as const) {}
