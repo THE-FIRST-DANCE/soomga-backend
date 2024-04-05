7fd962ebcab4cb6bcb2311880cac30ddec1d48d1
Juhyeon Lee <l.juhyeonni@gmail.com>
2024-04-05 16:49:02 +0900

    fix(constants): error message 추가

--- src/shared/constants/error-messages.constants.ts
+++ src/shared/constants/error-messages.constants.ts
@@ -9,6 +9,7 @@ enum ErrorMessage {
   NOT_REGISTERED = '가입되지 않은 사용자입니다.',
   NO_CHANGES = '변경된 내용이 없습니다.',
   FORBIDDEN_MEMBER = '탈퇴한 회원이거나 비활성화된 회원입니다.',
+  NOTFOUND_REVIEW = '리뷰를 찾을 수 없습니다.',
   PERMISSION_DENIED = '권한이 없습니다.',
   PHONE_NUMBER_ALREADY_EXISTS = '이미 등록된 전화번호입니다.',
   INVALID_AUTH_CODE = '인증코드가 일치하지 않습니다.',
