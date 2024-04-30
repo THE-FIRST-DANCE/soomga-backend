ce2a60ae2aaf92d246b1031cac54cfeabf20f2ed
hetame1 <poi8946@naver.com>
2024-04-30 09:05:43 +0900

    feat: trip 만들때 지역값 받기

--- src/modules/boards/trips/dto/create-trip.dto.ts
+++ src/modules/boards/trips/dto/create-trip.dto.ts
@@ -1,7 +1,13 @@
 import { ApiProperty, OmitType } from '@nestjs/swagger';
 import { CreateBoardDto } from 'src/shared/boards/dto/create-board.dto';
+import { IsNotEmpty, IsNumber } from 'class-validator';
 
 export class CreateTripDto extends OmitType(CreateBoardDto, ['type'] as const) {
   @ApiProperty()
   tags: string[];
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  areaId: number;
 }
